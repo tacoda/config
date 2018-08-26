a2c8e7703346e425a6e164bc6c0e3564b94e7c0b
Ian Johnson <tacoda@pm.me>
2018-08-26 16:46:46 -0400

    Rework setup structure

--- config.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-source cli-tools.sh
-source fish-config.sh
-source atom-config.sh
-source hyper-config.sh
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+source setup/cli.sh
+source setup/fish.sh
+source setup/atom.sh
+source setup/hyper.sh
