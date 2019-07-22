11421bc8a86eb0a9bc25c8d82a1d11efc5c7a8ea
Ian Johnson <ijohnson@covermymeds.com>
2019-07-21 22:11:34 -0400

    Add extra line return between date and diff output

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ if [[ -e $HOME/.spacemacs ]]; then
     mv $HOME/.spacemacs $HOME/.spacemacs-old
     cp spacemacs/spacemacs $HOME/.spacemacs
     date > $HOME/.spacemacs-lid
+    echo "\n" > $HOME/.spacemacs-lid
     diff $HOME/.spacemacs $HOME/.spacemacs-old >> $HOME/.spacemacs-lid
 fi
 
@@ -11,6 +12,7 @@ if [[ -e $HOME/.gitignore_global ]]; then
     mv $HOME/.gitignore_global $HOME/.gitignore_global-old
     cp git/gitignore_global $HOME/.gitignore_global
     date > $HOME/.gitignore_global-lid
+    echo "\n" > $HOME/.gitignore_global-lid
     diff $HOME/.gitignore_global $HOME/.gitignore_global-old >> $HOME/.gitignore_global-lid
 fi
 
