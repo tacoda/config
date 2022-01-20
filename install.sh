5645524addb92075e51e36afa2f193776127f50c
Ian Johnson <ijohnson@covermymeds.com>
2022-01-20 12:49:47 -0500

    Add brewfile and update install script

--- install.sh
+++ install.sh
@@ -2,23 +2,22 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# deps() {
-    # echo "Installing dependencies:"
-    # echo "➜ docker neovim emacs"
-    # dnf install -y docker neovim python3-neovim git make automake gcc gcc-c++ kernel-devel
-    # echo "➜ bat, chruby, elixir, emacs-plus, exa, fd, fzf, gcc, ripgrep, neovim, node, starship"
-    # bat, exa, fd, fzf, ripgrep, starship
-    # echo "✓ Dependencies installed!"
-# }
-# deps
+deps() {
+    echo "➜ Installing Brewfile"
+    mkdir -p $HOME/.config/brew
+    cp brew/Brewfile $HOME/.config/brew/Brewfile
+    echo "➜ Installing Brew Packages"
+    (cd $HOME/.config/brew && brew bundle)
+}
 
 install() {
     echo "Installing dot-files:"
     echo "➜ ZSH"
     cp zsh/zshrc $HOME/.zshrc
     cp zsh/zsh_aliases $HOME/.zsh_aliases
-    # echo "➜ Starship"
-    # cp starship/starship.toml $HOME/.config/starship.toml
+    echo "➜ Starship"
+    cp starship/starship.toml $HOME/.config/starship.toml
+    # echo "➜ Neovim"
     # echo "➜ Emacs"
     # cp -R emacs/ $HOME/.emacs.d
     # echo "➜ Git"
@@ -26,4 +25,11 @@ install() {
     echo "✓ Configurations installed!"
 }
 
+reload() {
+    source $HOME/.zshrc
+    echo "✓ Configurations reloaded!"
+}
+
+deps
 install
+reload
