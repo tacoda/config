bde1902b5fdbfe136c83f3c1e17c5e895079f700
Ian Johnson <tacoda@hey.com>
2023-02-06 17:39:10 -0500

    Swap brew for nix

--- install.sh
+++ install.sh
@@ -10,15 +10,41 @@ source $HOME/.nix-profile/etc/profile.d/nix.sh
 
 # Install packages
 nix profile install \
-	"nixpkgs#neovim" \
-	"nixpkgs#tmux" \
+	"nixpkgs#bat" \
+	"nixpkgs#direnv" \
 	"nixpkgs#emacs" \
 	"nixpkgs#fzf" \
+	"nixpkgs#grex" \
 	"nixpkgs#git" \
+	"nixpkgs#hugo" \
+	"nixpkgs#jq" \
 	"nixpkgs#kakoune" \
+	"nixpkgs#neovim" \
 	"nixpkgs#ripgrep" \
-	"nixpkgs#direnv" \
+	"nixpkgs#tmux" \
 	"nixpkgs#zsh"
+
+# brew "asdf"
+# brew "bat"
+# brew "delta"
+# brew "dust"
+# brew "fd"
+# brew "fzf"
+# brew "grex"
+# brew "http-server"
+# brew "hugo"
+# brew "jq"
+# brew "lsd"
+# brew "kakoune"
+# brew "procs"
+# brew "ripgrep"
+# brew "sd"
+# brew "starship"
+# brew "tealdeer"
+# brew "tokei"
+# brew "xsv"
+# brew "yq"
+# brew "zellij"
 
 # Add zsh to valid login shells
 command -v zsh | sudo tee -a /etc/shells
