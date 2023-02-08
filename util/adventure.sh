2288acc70de68635c94e6f1bc5f12cf308249b06
Ian Johnson <tacoda@hey.com>
2023-02-07 22:09:34 -0500

    Update indentation

--- util/adventure.sh
+++ util/adventure.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 main() {
-	verify_deps
-	repl
+    verify_deps
+    repl
 }
 
 verify_deps() {
@@ -14,54 +14,54 @@ verify_deps() {
 }
 
 repl() {
-	states=('red' 'green' 'blue' 'describe-spec')
-	while true; do
-		echo
-		echo "-----"
-		echo "Available states:"
-		echo "-----"
-		for s in "${states[@]}"; do
-			echo "$s"
-		done
-		echo "====="
-		echo -n ">> "
-		read -r state
-		delegate "$state"
+    states=('red' 'green' 'blue' 'describe-spec')
+    while true; do
+	echo
+	echo "-----"
+	echo "Available states:"
+	echo "-----"
+	for s in "${states[@]}"; do
+		echo "$s"
 	done
+	echo "====="
+	echo -n ">> "
+	read -r state
+	delegate "$state"
+    done
 }
 
 delegate() {
-	case "$1" in
-		describe-spec)
-			describe_spec
-			;;
+    case "$1" in
+	describe-spec)
+	    describe_spec
+	    ;;
 
-		red | ebb)
-			echo "red"
-			;;
+	red | ebb)
+	    echo "red"
+	    ;;
 
-		green | flow)
-			echo "green"
-			;;
+	green | flow)
+	    echo "green"
+	    ;;
 
-		blue | zen | refactor)
-			echo "blue"
-			;;
+	blue | zen | refactor)
+	    echo "blue"
+	    ;;
 
-		exit | quit)
-			exit 0
-			;;
+	exit | quit)
+	    exit 0
+	    ;;
 
-		*)
-			echo "No such state"
-			;;
-	esac
+	*)
+	    echo "No such state"
+	    ;;
+    esac
 }
 
 describe_spec() {
-	echo "[Describe a spec]"
-	file=$(fzf)
-	rg "describe|context|it" $file
+    echo "[Describe a spec]"
+    file=$(fzf)
+    rg "describe|context|it" $file
 }
 
 main
